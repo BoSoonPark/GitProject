fe8f3b62ca7f6aa2df6802ad3920de5058adc9b4
bpark <bpark@confluent.io>
2022-07-29 09:05:08 +0900

    Initial Commit

---
+++ src/com/mycompany/GitProject.java
@@ -0,0 +1,10 @@
+package com.mycompany;
+
+/**
+ * Created by bpark on 2022/07/29
+ */
+public class GitProject {
+    public static void main(String[] args) {
+        System.out.println("Hello from Git!");
+    }
+}
